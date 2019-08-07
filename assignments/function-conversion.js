6cd623cf560bb557514b54b98096089884ea1f94
CodingCCarpenter <christineassists@aol.com>
2019-08-07 11:28:50 -0500

    problem one functions conversion dot js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -3,12 +3,16 @@
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
-// myFunction();
+let myFunction = () => {console.log("Function was invoked!");}
+myFunction();
+
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
-// anotherFunction("Example");
+let anotherFunction = (param) => {return param;}
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
