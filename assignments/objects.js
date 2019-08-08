89e3358688820a164d401a58716d46684c31487c
CodingCCarpenter <christineassists@aol.com>
2019-08-07 20:58:44 -0500

    complete kennan speak and antionetta multiply

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,35 +19,35 @@ const example = {
 
 // Write your intern objects here:
 // 1,mmelloy0@psu.edu,Mitzi,F
-const Mitzi = {
+const mitzi = {
   id: 1,
   name: "Mitzi",
   email: "mmelloy@psu.edu", 
   gender: "F"
 }
 // 2,kdiben1@tinypic.com,Kennan,M
-const Kennan = {
+const kennan = {
   id: 2,
   name: "Kennan",
   email: "kbiden1@tinypic.com",
   gender: "M"
 }
 // 3,kmummery2@wikimedia.org,Keven,M
-const Mitzi = {
+const keven = {
   id: 3,
   name: "Keven",
   email: "kmummery2@wikimedia.org", 
   gender: "M"
 }
 // 4,gmartinson3@illinois.edu,Gannie,M
-const Mitzi = {
+const gannie = {
   id: 4,
   name: "Gannie",
   email: "gmartisnson3@illinois.edu", 
   gender: "M"
 }
 // 5,adaine5@samsung.com,Antonietta,F
-const Antonietta = {
+const antonietta = {
   id: 5,
   name: "Antonietta",
   email: "adaine5@samsung.com", 
@@ -58,21 +58,28 @@ const Antonietta = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-console.log(Mitzi["name"]);
+console.log(mitzi["name"]);
 // Kennan's ID
-
+console.log(kennan["id"]);
 // Keven's email
-
+console.log(keven["email"]);
 // Gannie's name
-
+console.log(gannie["name"]);
 // Antonietta's Gender
-
+console.log(antonietta["gender"]);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+kennan.speak = function(){
+  console.log( `Hello, my name is ${this.name}!`);
+}
+console.log(kennan.speak());
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+antonietta.multiply = function(num1, num2){
+  return num1 * num2;
+}
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
